9fb96e4e8a7afd04cf3419975dd7ae2903b300c9
aarmingames <61307644+aarmingames@users.noreply.github.com>
2021-01-24 09:34:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Aarmin";
+var last_name = "Bhatnagar";
+
+console.log("My first project!");
+
+var concat_name = first_name.concat(last_name);
+console.log(concat_name);
+
+var num1 = 10;
+var num2 = 50;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
